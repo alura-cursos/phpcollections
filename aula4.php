121210cf1ab25602cd8843e38fb7b1591c9f0b54
Giovanni Tempobono <giovanni@gtbono.com>
2019-01-30 12:05:57 -0200

    refatorada

--- aula4.php
+++ aula4.php
@@ -1,31 +1,36 @@
 <?php
 
-require 'MaisTocadas.php';
-require 'Musica.php';
+require 'Ranking.php';
 require 'TocadorDeMusica.php';
+require 'Musica.php';
 
 $musicas = new SplFixedArray(4);
-$musicas[0] = new Musica("wonderful life");
-$musicas[1] = new Musica("amo");
-$musicas[2] = new Musica("Closer");
+$musicas[0] = new Musica("One Dance");
+$musicas[1] = new Musica("Closer");
+$musicas[2] = new Musica("rockstar");
 $musicas[3] = new Musica("Love Yourself");
 
 $tocador = new TocadorDeMusica();
+
 $tocador->adicionarMusicas($musicas);
 
 $tocador->tocarMusica();
 $tocador->tocarMusica();
-
-$tocador->avancarMusica();
-
-$tocador->tocarMusica();
-$tocador->tocarMusica();
 $tocador->tocarMusica();
 $tocador->tocarMusica();
 $tocador->tocarMusica();
 
 $tocador->avancarMusica();
 
+
+
+$tocador->tocarMusica();
+$tocador->tocarMusica();
+
+
+$tocador->avancarMusica();
+
+
 $tocador->tocarMusica();
 $tocador->tocarMusica();
 $tocador->tocarMusica();
@@ -33,5 +38,6 @@ $tocador->tocarMusica();
 $tocador->avancarMusica();
 
 $tocador->tocarMusica();
+$tocador->tocarMusica();
 
-$tocador->exibirMaisTocadas();
+$tocador->exibirRanking();
